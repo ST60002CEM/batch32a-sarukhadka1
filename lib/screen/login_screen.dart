73f5da4b76f86dad54198ff41022a29c66a80caa
sarukhadka1 <saru.khadka521@gmail.com>
2024-05-17 23:46:06 +0545

    changed

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -256,8 +256,8 @@ class LoginScreen extends StatelessWidget {
         children: [
           Image.asset(
             asset,
-            height: 20.0,
-            width: 20.0,
+            height: 24.0,
+            width: 24.0,
           ),
           const SizedBox(width: 8.0),
         ],
