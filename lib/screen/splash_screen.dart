0741e5a602810ce395d170a5e175ac76ad9e8871
sarukhadka1 <saru.khadka521@gmail.com>
2024-05-17 23:54:43 +0545

    splash added

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -51,3 +51,36 @@ class _SplashScreenState extends State<SplashScreen> {
     );
   }
 }
+
+@override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/logo.png', // Your image path here
+              width: MediaQuery.of(context).size.width *
+                  0.5, // Adjust size as needed
+              height: MediaQuery.of(context).size.height *
+                  0.3, // Adjust size as needed
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Welcome to Glam Hire',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            const SizedBox(height: 20),
+            const CircularProgressIndicator(),
+          ],
+        ),
+      ),
+    );
+  }
+}
