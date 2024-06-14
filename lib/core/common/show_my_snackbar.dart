580fc65fffa89fd2b14266f6f1920cc34616e3b1
sarukhadka1 <saru.khadka521@gmail.com>
2024-06-14 16:14:32 +0545

    colour changed

--- lib/core/common/show_my_snackbar.dart
+++ lib/core/common/show_my_snackbar.dart
@@ -12,10 +12,9 @@ showMySnackBar({
   ).showSnackBar(
     SnackBar(
       content: Text(message),
-      backgroundColor: color ?? Colors.green,
+      backgroundColor: color ?? Colors.purple,
       duration: const Duration(seconds: 1),
       behavior: SnackBarBehavior.floating,
     ),
   );
 }
-
