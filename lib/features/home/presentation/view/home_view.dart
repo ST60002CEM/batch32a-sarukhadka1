b414f54a71d415d250d2e6fea98bbeb1215241d5
sarukhadka1 <saru.khadka521@gmail.com>
2024-08-08 17:49:36 +0545

    home view

--- lib/features/home/presentation/view/home_view.dart
+++ lib/features/home/presentation/view/home_view.dart
@@ -4,10 +4,11 @@ import 'package:all_sensors2/all_sensors2.dart';
 import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:final_assignment/core/common/show_my_snackbar.dart';
 import 'package:final_assignment/features/home/presentation/view/bottom_view/dashboard_view.dart';
-import 'package:final_assignment/features/home/presentation/view/bottom_view/homee_view.dart';
 import 'package:final_assignment/features/home/presentation/view/bottom_view/profile_view.dart';
-import 'package:final_assignment/features/home/presentation/view/bottom_view/setting_view.dart';
+import 'package:final_assignment/features/home/presentation/view/bottom_view/wishlist_view.dart';
+import 'package:final_assignment/features/home/presentation/view/bottom_view/booking_view.dart';
 import 'package:final_assignment/features/home/presentation/viewmodel/home_view_model.dart';
+import 'package:final_assignment/features/profile/presentation/view/profile_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -22,17 +23,18 @@ class _HomeViewState extends ConsumerState<HomeView> {
   int selectedIndex = 0;
   List<Widget> lstScreen = [
     const DashboardView(),
-    const ProfileView(),
-    const SettingView(),
-    const HomeeView()
+    const WishlistView(),
+    const BookingView(),
+    //const HomeeView(),
+    const ProfileView()
   ];
-  // bool showYesNoDialog = true;
-  // bool isDialogShowing = false;
+  bool showYesNoDialog = true;
+  bool isDialogShowing = false;
 
-  // List<double> _gyroscopeValues = [];
-  // final List<StreamSubscription<dynamic>> _streamSubscription = [];
+  List<double> _gyroscopeValues = [];
+  final List<StreamSubscription<dynamic>> _streamSubscription = [];
 
-  // @override
+  @override
   // void initState() {
   //   _streamSubscription.add(gyroscopeEvents!.listen((GyroscopeEvent event) {
   //     setState(() {
@@ -83,20 +85,20 @@ class _HomeViewState extends ConsumerState<HomeView> {
             label: 'Dashboard',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
+            icon: Icon(Icons.favorite),
+            label: 'Wislist',
           ),
           // BottomNavigationBarItem(
           //   icon: Icon(Icons.home),
           //   label: 'Home',
           // ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'Setting',
+            icon: Icon(Icons.book),
+            label: 'Bookings',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
-            label: 'Home',
+            label: 'Profile',
           ),
         ],
         currentIndex: selectedIndex,
