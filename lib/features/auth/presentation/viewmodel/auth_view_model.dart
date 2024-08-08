3f87fe1776b7b0c842e51032233a7d2f4db0c7fd
sarukhadka1 <saru.khadka521@gmail.com>
2024-08-08 17:49:16 +0545

    auth view

--- lib/features/auth/presentation/viewmodel/auth_view_model.dart
+++ lib/features/auth/presentation/viewmodel/auth_view_model.dart
@@ -5,6 +5,7 @@ import 'package:final_assignment/features/auth/presentation/navigator/login_navi
 import 'package:final_assignment/features/auth/presentation/state/auth_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:local_auth/local_auth.dart';
 
 final authViewModelProvider = StateNotifierProvider<AuthViewModel, AuthState>(
   (ref) => AuthViewModel(
@@ -17,23 +18,24 @@ class AuthViewModel extends StateNotifier<AuthState> {
   AuthViewModel(this.navigator, this.authUseCase) : super(AuthState.initial());
   final AuthUseCase authUseCase;
   final LoginViewNavigator navigator;
+  late LocalAuthentication _localAuth;
 
   Future<void> createUser(AuthEntity user) async {
     state = state.copyWith(isLoading: true);
     var data = await authUseCase.createUser(user);
-    // data.fold(
-    //   (failure) {
-    //     state = state.copyWith(
-    //       isLoading: false,
-    //       error: failure.error,
-    //     );
-    //     showMySnackBar(message: failure.error, color: Colors.red);
-    //   },
-    //   (success) {
-    //     state = state.copyWith(isLoading: false, error: null);
-    //     showMySnackBar(message: "Successfully registered");
-    //   },
-    // );
+    data.fold(
+      (failure) {
+        state = state.copyWith(
+          isLoading: false,
+          error: failure.error,
+        );
+        showMySnackBar(message: failure.error, color: Colors.red);
+      },
+      (success) {
+        state = state.copyWith(isLoading: false, error: null);
+        showMySnackBar(message: "Successfully registered");
+      },
+    );
   }
 
   Future<void> loginUser(
@@ -42,17 +44,17 @@ class AuthViewModel extends StateNotifier<AuthState> {
   ) async {
     state = state.copyWith(isLoading: true);
     var data = await authUseCase.loginUser(email, password);
-    // data.fold(
-    //   (failure) {
-    //     state = state.copyWith(isLoading: false, error: failure.error);
-    //     showMySnackBar(message: failure.error, color: Colors.red);
-    //   },
-    //   (success) {
-    //     state = state.copyWith(isLoading: false, error: null);
-    //     showMySnackBar(message: "Logged in");
-    //     openHomeView();
-    //   }, 
-    // );
+    data.fold(
+      (failure) {
+        state = state.copyWith(isLoading: false, error: failure.error);
+        showMySnackBar(message: failure.error, color: Colors.red);
+      },
+      (success) {
+        state = state.copyWith(isLoading: false, error: null);
+        showMySnackBar(message: "Logged in");
+        openHomeView();
+      }, 
+    );
   }
 
   void openRegisterView() {
@@ -61,5 +63,43 @@ class AuthViewModel extends StateNotifier<AuthState> {
 
   void openHomeView() {
     navigator.openHomeView();
+  }
+  Future<void> fingerPrintLogin() async {
+    _localAuth = LocalAuthentication();
+ 
+    bool authenticated = false;
+    try {
+      authenticated = await _localAuth.authenticate(
+        localizedReason: 'Authenticate to enable fingerprint',
+        options: const AuthenticationOptions(
+          stickyAuth: true,
+          biometricOnly: true,
+          useErrorDialogs: true,
+        ),
+      );
+    } catch (e) {
+      showMySnackBar(
+          message: 'Fingerprint authentication failed', color: Colors.red);
+    }
+ 
+    if (authenticated) {
+      authUseCase.fingerPrintLogin().then((data) {
+        data.fold(
+          (l) {
+            showMySnackBar(message: l.error, color: Colors.red);
+          },
+          (r) {
+            showMySnackBar(message: "User logged in successfully");
+            navigator.openHomeView();
+          },
+        );
+      });
+    } else {
+      showMySnackBar(
+          message: 'Fingerprint authentication failed', color: Colors.red);
+    }
+  }
+  void logout() async {
+    navigator.openLoginView();
   }
 }
