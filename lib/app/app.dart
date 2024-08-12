d86ee61449ed2ec8faab0514746631f3453b1bb4
sarukhadka1 <saru.khadka521@gmail.com>
2024-08-12 15:55:34 +0545

    app dart

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,7 @@
 import 'package:final_assignment/app/navigator_key/navigator_key.dart';
 import 'package:final_assignment/app/themes/app_theme.dart';
 import 'package:final_assignment/core/common/provider/theme_view_model_provider.dart';
+import 'package:final_assignment/features/booking/presentation/view/booking_view.dart';
 import 'package:final_assignment/features/splash/presentation/view/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
