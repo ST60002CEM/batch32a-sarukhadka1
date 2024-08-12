acc53788a5ef6a24bf89796a2fa6877683f1e108
sarukhadka1 <saru.khadka521@gmail.com>
2024-08-12 15:55:51 +0545

    api endpoint

--- lib/app/constants/api_endpoint.dart
+++ lib/app/constants/api_endpoint.dart
@@ -22,4 +22,17 @@ class ApiEndpoints {
 
   // static const String imageUrl = 'http://192.168.1.133:5000/products/'
   static const String imageUrl = 'http://10.0.2.2:5000/artists/';
+
+
+  // ===========Booking Routes===========
+  static const String createBooking = 'booking/bookings';
+  static const String getAllBookings = 'booking/all_bookings';
+  static const String getUserBookings = 'booking/mybookings';
+  static const String updateBookingStatus = 'booking/bookings/status';
+  static const String updatePaymentMethod = 'booking/bookings/payment';
+
+  // wishlist
+  static const String getUserFavorites = 'wishlist/all';
+  static const String addFavorite = 'wishlist/add';
+  static const String deleteFavorite = 'wishlist/remove';
 }
