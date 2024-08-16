8d3df9a1e28d64c87738a327f042f10f503f78a3
sarukhadka1 <saru.khadka521@gmail.com>
2024-08-16 12:22:09 +0545

    my artist cart

--- lib/core/common/widgets/my_artist_cart.dart
+++ lib/core/common/widgets/my_artist_cart.dart
@@ -1,127 +1,127 @@
-import 'package:final_assignment/app/constants/api_endpoint.dart';
-import 'package:final_assignment/features/booking/presentation/view/booking_view.dart';
-import 'package:final_assignment/features/home/domain/entity/artist_entity.dart';
-import 'package:flutter/material.dart';
+// import 'package:final_assignment/app/constants/api_endpoint.dart';
+// import 'package:final_assignment/features/booking/presentation/view/booking_view.dart';
+// import 'package:final_assignment/features/home/domain/entity/artist_entity.dart';
+// import 'package:flutter/material.dart';
 
-class MyArtistCard extends StatelessWidget {
-  final ArtistEntity artist;
-  final bool isDark;
+// class MyArtistCard extends StatelessWidget {
+//   final ArtistEntity artist;
+//   final bool isDark;
 
-  const MyArtistCard({required this.artist, required this.isDark, super.key});
+//   const MyArtistCard({required this.artist, required this.isDark, super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    final imageUrl = '${ApiEndpoints.imageUrl}${artist.artistImage}';
+//   @override
+//   Widget build(BuildContext context) {
+//     final imageUrl = '${ApiEndpoints.imageUrl}${artist.artistImage}';
 
-    return Container(
-      decoration: BoxDecoration(
-        color: isDark
-            ? Colors.grey[850]
-            : Colors.white, // Adjust background color for dark mode
-        borderRadius: BorderRadius.circular(16),
-        boxShadow: [
-          BoxShadow(
-            color: isDark
-                ? Colors.black26
-                : Colors.black12, // Adjust shadow color for dark mode
-            blurRadius: 4,
-            spreadRadius: 2,
-          ),
-        ],
-      ),
-      child: Stack(
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(
-                child: ClipRRect(
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(16),
-                    topRight: Radius.circular(16),
-                  ),
-                  child: Image.network(
-                    imageUrl,
-                    fit: BoxFit.cover,
-                    width: double.infinity,
-                    height: double.infinity,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      artist.artistName,
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: isDark
-                            ? Colors.white
-                            : Colors.black, // Adjust text color for dark mode
-                      ),
-                    ),
-                    const SizedBox(height: 4),
-                    Text(
-                      artist.artistDescription,
-                      style: TextStyle(
-                        fontSize: 14,
-                        color: isDark
-                            ? Colors.grey[400]
-                            : Colors.grey, // Adjust text color for dark mode
-                      ),
-                    ),
-                    // const SizedBox(height: 4),
-                    // Text(
-                    //   artist.artistGenre,
-                    //   style: TextStyle(
-                    //     fontSize: 14,
-                    //     color: isDark
-                    //         ? Colors.grey[400]
-                    //         : Colors.grey, // Adjust text color for dark mode
-                    //   ),
-                    // ),
-                    // Text(
-                    //   artist.artistRate.toString(),
-                    //   style: TextStyle(
-                    //     fontSize: 14,
-                    //     color: isDark
-                    //         ? Colors.grey[400]
-                    //         : Colors.grey, // Adjust text color for dark mode
-                    //   ),
-                    // ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-          Positioned(
-            bottom: 0,
-            right: 0,
-            child: IconButton(
-              icon: Icon(
-                Icons.book_online,
-                color: isDark
-                    ? Colors.purpleAccent
-                    : Colors.purple, // Adjust icon color for dark mode
-              ),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const BookingView(),
-                  ),
-                );
-              },
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
+//     return Container(
+//       decoration: BoxDecoration(
+//         color: isDark
+//             ? Colors.grey[850]
+//             : Colors.white, // Adjust background color for dark mode
+//         borderRadius: BorderRadius.circular(16),
+//         boxShadow: [
+//           BoxShadow(
+//             color: isDark
+//                 ? Colors.black26
+//                 : Colors.black12, // Adjust shadow color for dark mode
+//             blurRadius: 4,
+//             spreadRadius: 2,
+//           ),
+//         ],
+//       ),
+//       child: Stack(
+//         children: [
+//           Column(
+//             crossAxisAlignment: CrossAxisAlignment.start,
+//             children: [
+//               Expanded(
+//                 child: ClipRRect(
+//                   borderRadius: const BorderRadius.only(
+//                     topLeft: Radius.circular(16),
+//                     topRight: Radius.circular(16),
+//                   ),
+//                   child: Image.network(
+//                     imageUrl,
+//                     fit: BoxFit.cover,
+//                     width: double.infinity,
+//                     height: double.infinity,
+//                   ),
+//                 ),
+//               ),
+//               Padding(
+//                 padding: const EdgeInsets.all(8),
+//                 child: Column(
+//                   crossAxisAlignment: CrossAxisAlignment.start,
+//                   children: [
+//                     Text(
+//                       artist.artistName,
+//                       style: TextStyle(
+//                         fontSize: 16,
+//                         fontWeight: FontWeight.bold,
+//                         color: isDark
+//                             ? Colors.white
+//                             : Colors.black, // Adjust text color for dark mode
+//                       ),
+//                     ),
+//                     const SizedBox(height: 4),
+//                     Text(
+//                       artist.artistDescription,
+//                       style: TextStyle(
+//                         fontSize: 14,
+//                         color: isDark
+//                             ? Colors.grey[400]
+//                             : Colors.grey, // Adjust text color for dark mode
+//                       ),
+//                     ),
+//                     // const SizedBox(height: 4),
+//                     // Text(
+//                     //   artist.artistGenre,
+//                     //   style: TextStyle(
+//                     //     fontSize: 14,
+//                     //     color: isDark
+//                     //         ? Colors.grey[400]
+//                     //         : Colors.grey, // Adjust text color for dark mode
+//                     //   ),
+//                     // ),
+//                     // Text(
+//                     //   artist.artistRate.toString(),
+//                     //   style: TextStyle(
+//                     //     fontSize: 14,
+//                     //     color: isDark
+//                     //         ? Colors.grey[400]
+//                     //         : Colors.grey, // Adjust text color for dark mode
+//                     //   ),
+//                     // ),
+//                   ],
+//                 ),
+//               ),
+//             ],
+//           ),
+//           Positioned(
+//             bottom: 0,
+//             right: 0,
+//             child: IconButton(
+//               icon: Icon(
+//                 Icons.book_online,
+//                 color: isDark
+//                     ? Colors.purpleAccent
+//                     : Colors.purple, // Adjust icon color for dark mode
+//               ),
+//               onPressed: () {
+//                 Navigator.push(
+//                   context,
+//                   MaterialPageRoute(
+//                     builder: (context) =>  BookingView(artist.id!),
+//                   ),
+//                 );
+//               },
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
 
 // import 'package:final_assignment/app/constants/api_endpoint.dart';
 // import 'package:final_assignment/features/home/domain/entity/artist_entity.dart';
@@ -241,6 +241,151 @@ class MyArtistCard extends StatelessWidget {
 //     );
 //   }
 // }
+
+
+
+
+
+
+
+import 'package:final_assignment/app/constants/api_endpoint.dart';
+import 'package:final_assignment/features/booking/presentation/view/booking_view.dart';
+import 'package:final_assignment/features/home/domain/entity/artist_entity.dart';
+import 'package:final_assignment/features/single_artist/presentation/widgets/my_single_artist.dart';
+import 'package:flutter/material.dart';
+ // Import SingleView
+
+class MyArtistCard extends StatelessWidget {
+  final ArtistEntity artist;
+  final bool isDark;
+
+  const MyArtistCard({required this.artist, required this.isDark, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final imageUrl = '${ApiEndpoints.imageUrl}${artist.artistImage}';
+
+    return Container(
+      decoration: BoxDecoration(
+        color: isDark
+            ? Colors.grey[850]
+            : Colors.white, // Adjust background color for dark mode
+        borderRadius: BorderRadius.circular(16),
+        boxShadow: [
+          BoxShadow(
+            color: isDark
+                ? Colors.black26
+                : Colors.black12, // Adjust shadow color for dark mode
+            blurRadius: 4,
+            spreadRadius: 2,
+          ),
+        ],
+      ),
+      child: Stack(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => SingleView(
+                          singleArtist: artist,
+                          selectedQuantity: 1, // You can set the initial quantity here
+                          onAddToBag: () {
+                            // Implement add to bag functionality
+                          },
+                          onIncreaseQuantity: () {
+                            // Implement increase quantity functionality
+                          },
+                          onDecreaseQuantity: () {
+                            // Implement decrease quantity functionality
+                          },
+                        ),
+                      ),
+                    );
+                  },
+                  child: ClipRRect(
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(16),
+                      topRight: Radius.circular(16),
+                    ),
+                    child: Image.network(
+                      imageUrl,
+                      fit: BoxFit.cover,
+                      width: double.infinity,
+                      height: double.infinity,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      artist.artistName,
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: isDark
+                            ? Colors.white
+                            : Colors.black, // Adjust text color for dark mode
+                      ),
+                    ),
+                    const SizedBox(height: 4),
+                    Text(
+                      artist.artistDescription,
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: isDark
+                            ? Colors.grey[400]
+                            : Colors.grey, // Adjust text color for dark mode
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Positioned(
+            bottom: 0,
+            right: 0,
+            child: IconButton(
+              icon: Icon(
+                Icons.book_online,
+                color: isDark
+                    ? Colors.purpleAccent
+                    : Colors.purple, // Adjust icon color for dark mode
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => BookingView(artist.id!),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
 
 
 
