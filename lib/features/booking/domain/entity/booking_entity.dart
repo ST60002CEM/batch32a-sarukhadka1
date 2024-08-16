19320370112c041df756d3aa335238ca858dd244
sarukhadka1 <saru.khadka521@gmail.com>
2024-08-16 12:23:48 +0545

    bookign entity

--- lib/features/booking/domain/entity/booking_entity.dart
+++ lib/features/booking/domain/entity/booking_entity.dart
@@ -3,7 +3,7 @@ import 'package:final_assignment/features/auth/domain/entity/auth_entity.dart';
 
 class BookingEntity extends Equatable {
   final String? id;
-  final String user;
+  final String? user;
   final String artist;
   final DateTime date;
   final String time;
@@ -13,7 +13,7 @@ class BookingEntity extends Equatable {
 
   const BookingEntity({
     this.id,
-    required this.user,
+    this.user,
     required this.artist,
     required this.date,
     required this.time,
