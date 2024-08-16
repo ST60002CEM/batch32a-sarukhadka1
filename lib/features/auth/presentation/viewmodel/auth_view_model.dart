57b515ff6e1d540c672f2d72c8a4b30d091ead7a
sarukhadka1 <saru.khadka521@gmail.com>
2024-08-16 18:18:14 +0545

    test

--- lib/features/auth/presentation/viewmodel/auth_view_model.dart
+++ lib/features/auth/presentation/viewmodel/auth_view_model.dart
@@ -23,19 +23,19 @@ class AuthViewModel extends StateNotifier<AuthState> {
   Future<void> createUser(AuthEntity user) async {
     state = state.copyWith(isLoading: true);
     var data = await authUseCase.createUser(user);
-    data.fold(
-      (failure) {
-        state = state.copyWith(
-          isLoading: false,
-          error: failure.error,
-        );
-        showMySnackBar(message: failure.error, color: Colors.red);
-      },
-      (success) {
-        state = state.copyWith(isLoading: false, error: null);
-        showMySnackBar(message: "Successfully registered");
-      },
-    );
+    // data.fold(
+    //   (failure) {
+    //     state = state.copyWith(
+    //       isLoading: false,
+    //       error: failure.error,
+    //     );
+    //     showMySnackBar(message: failure.error, color: Colors.red);
+    //   },
+    //   (success) {
+    //     state = state.copyWith(isLoading: false, error: null);
+    //     showMySnackBar(message: "Successfully registered");
+    //   },
+    // );
   }
 
   Future<void> loginUser(
@@ -44,17 +44,17 @@ class AuthViewModel extends StateNotifier<AuthState> {
   ) async {
     state = state.copyWith(isLoading: true);
     var data = await authUseCase.loginUser(email, password);
-    data.fold(
-      (failure) {
-        state = state.copyWith(isLoading: false, error: failure.error);
-        showMySnackBar(message: failure.error, color: Colors.red);
-      },
-      (success) {
-        state = state.copyWith(isLoading: false, error: null);
-        showMySnackBar(message: "Logged in");
-        openHomeView();
-      }, 
-    );
+    // data.fold(
+    //   (failure) {
+    //     state = state.copyWith(isLoading: false, error: failure.error);
+    //     showMySnackBar(message: failure.error, color: Colors.red);
+    //   },
+    //   (success) {
+    //     state = state.copyWith(isLoading: false, error: null);
+    //     showMySnackBar(message: "Logged in");
+    //     openHomeView();
+    //   }, 
+    // );
   }
 
   void openRegisterView() {
